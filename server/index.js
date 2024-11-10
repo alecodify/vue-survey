faa1d3a48872694d5cce4ef39b64c5d7bb40a04d
aliraza <chaudaryaliraza0@gmail.com>
2024-11-10 12:37:54 +0500

    set

--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(cookieParser()); 
 app.use(cors({ 
-  origin: ['http://localhost:5173', 'https://vue-survey-client.vercel.app/'],
+  origin: ['https://vue-survey-client.vercel.app/', 'http://localhost:5173'],
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true,
 }))
