2abbd04d47050768c2a675c60d3fbaa209018ecb
aliraza <chaudaryaliraza0@gmail.com>
2024-11-10 11:15:25 +0500

    setup

--- client/vite.config.js
+++ client/vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:9000'
+        target: 'https://vue-survey-server.vercel.app/'
       }
     }
   },
